6f260f50fa205ceb7c21169ac557edc0e9ba94d0
AdmiralJo <admiraljo2013@gmail.com>
2017-01-29 21:21:51 +0100

    Added weather component

---
+++ src/test/TestWeatherMain.java
@@ -0,0 +1,27 @@
+package test;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.TimeZone;
+
+import components.Weather;
+
+public class TestWeatherMain {
+	private static final String API_KEY = "your token here";
+
+	public static void main(String[] args) {
+		Weather weather = new Weather(API_KEY);
+		weather.getForecastToday();
+
+		System.out.println("Min Temp:" + weather.getMinTemp());
+		System.out.println("Max Temp:" + weather.getMaxTemp());
+		System.out.println("Temp:" + weather.getTemp());
+		System.out.println("Weather: " + weather.getWeatherDescribtion());
+
+		// System.out.println(date.getDay() + "." + date.getMonth() + "." +
+		// date.getYear() + " " + date.getHours() + ":"
+		// + date.getMinutes());
+
+	}
+
+}
